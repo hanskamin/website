9e35b97eaafdaf83b850dfe3ab5008fe7888c94a
Hans Kamin <hansjkamin@gmail.com>
2024-12-25 18:14:50 -0500

    created the button component

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -1,16 +1,32 @@
+"use client";
+
 import clsx from "clsx";
+import { useRouter } from "next/navigation";
 
 type ButtonProps = {
   className?: string,
   title?: string,
+  route?: string,
 };
 
-export default function Button({ className, title }: ButtonProps) {
+export default function Button({ className, title, route }: ButtonProps) {
+  const router = useRouter();
+
+  const goToRoute = () => {
+    if (route) {
+      router.replace(route);
+    }
+  };
+
   return (
-    <div className={clsx(className)}>
-      <button>
-        {title}
-      </button>
-    </div>
+    <button
+      className={clsx(
+        "w-[200px] bg-[#000000] rounded-full py-3 text-center font-[family-name:var(--font-geist-mono)]",
+        className,
+      )}
+      onClick={goToRoute}
+    >
+      {title}
+    </button>
   );
 }
