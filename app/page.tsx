e958962929ec7f43133be7ba58629bba539f2a01
Hans Kamin <hansjkamin@gmail.com>
2024-12-25 18:12:04 -0500

    home page scaffolding

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,56 @@ import Button from './ui/Button';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <div className="text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-mono)]">
+      {/* Main Content Wrapper */}
+      <div className="flex flex-col items-center text-center">
         {/* Profile Image */}
-        <div className="pb-2 mx-auto mb-4 relative">
+        <div className="mb-4 relative flex justify-center">
           <Image
             src="/profile.jpeg"
             alt="Hans Kamin"
-            className="rounded-full shadow-lg"
-            width={250} // Fixed width
-            height={250} // Fixed height
-            // objectFit="cover" // Ensure proper cropping
+            className="rounded-full shadow-lg w-auto h-auto"
+            width={250}
+            height={250}
           />
         </div>
 
-        {/* Name */}
-        <h1 className="text-3xl font-semibold pb-2 font-[family-name:var(--font-geist-mono)]">Hans Kamin</h1>
+        {/* Text */}
+        <div className='py-4'>
+          {/* Name */}
+          <h1 className="text-3xl font-semibold pb-2">
+            Hans
+          </h1>
 
-        {/* Location */}
-        <p className="text-lg pb-5 font-[family-name:var(--font-geist-mono)]">New York, NY</p>
+          {/* Location */}
+          <p className="text-lg pb-2">
+            New York, NY
+          </p>
 
-        <Button title='Music' />
+          {/* Bio */}
+          <p className="text-md pb-5">
+            Musician, Tennis Player, Developer
+          </p>
+        </div>
+
+        {/* Buttons */}
+        <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-4">
+          <Button
+            className="py-4 px-6 rounded-full"
+            title={"👨🏾‍🎤 Music"}
+            route={"/music"}
+          />
+          <Button
+            className="py-4 px-6 text-white rounded-full"
+            title={"👨🏾‍💻 Software"}
+            route={"/software"}
+          />
+          <Button
+            className="py-4 px-6 rounded-full"
+            title={"🇺🇳 Volunteering"}
+            route={"/volunteering"}
+          />
+        </div>
       </div>
     </div>
   );
